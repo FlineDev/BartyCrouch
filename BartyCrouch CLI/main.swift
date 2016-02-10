74a27d97dfdddeb68443ee1d473a88a4b8b02fa5
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-02-10 01:47:40 +0100

    Configure targets

---
+++ BartyCrouch CLI/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  BartyCrouch CLI
+//
+//  Created by Cihat Gündüz on 10.02.16.
+//  Copyright © 2016 Flinesoft. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
