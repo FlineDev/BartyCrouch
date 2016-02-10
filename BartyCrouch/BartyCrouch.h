7c3e8c7bcd6d41f90e4cfc3d083a6545d74512e5
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-02-10 01:26:29 +0100

    Initial Commit

---
+++ BartyCrouch/BartyCrouch.h
@@ -0,0 +1,19 @@
+//
+//  BartyCrouch.h
+//  BartyCrouch
+//
+//  Created by Cihat Gündüz on 10.02.16.
+//  Copyright © 2016 Flinesoft. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for BartyCrouch.
+FOUNDATION_EXPORT double BartyCrouchVersionNumber;
+
+//! Project version string for BartyCrouch.
+FOUNDATION_EXPORT const unsigned char BartyCrouchVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <BartyCrouch/PublicHeader.h>
+
+
