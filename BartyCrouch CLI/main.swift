43723a549cdf34bed3366d5b81f5c17ba1d84646
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-02-11 23:58:11 +0100

    Read path arguments from command line

--- BartyCrouch CLI/main.swift
+++ BartyCrouch CLI/main.swift
@@ -8,5 +8,57 @@
 
 import Foundation
 
-print("Hello, World!")
+let currentPath = Process.arguments[0]
 
+let inputStoryboardArguments = ["--input-storyboard", "-in"]
+let outputStringsFilesArguments = ["--output-strings-files", "-out"]
+
+func run() {
+    
+    let inputStoryboardIndexOptional: Int? = {
+        for inputStoryboardArgument in inputStoryboardArguments {
+            if let index = Process.arguments.indexOf(inputStoryboardArgument) {
+                return index
+            }
+        }
+        return nil
+    }()
+    
+    guard let inputStoryboardIndex = inputStoryboardIndexOptional else {
+        print("missing input key '\(inputStoryboardArguments[0])' or '\(inputStoryboardArguments[1])'")
+        return
+    }
+    
+    guard inputStoryboardIndex+1 <= Process.arguments.count else {
+        print("missing input path after key '\(inputStoryboardArguments[0])' or '\(inputStoryboardArguments[1])'")
+        return
+    }
+    
+    let inputStoryboardPath = Process.arguments[inputStoryboardIndex+1]
+    
+    let outputStringsFilesIndexOptional: Int? = {
+        for outputStringsFilesArgument in outputStringsFilesArguments {
+            if let index = Process.arguments.indexOf(outputStringsFilesArgument) {
+                return index
+            }
+        }
+        return nil
+    }()
+    
+    guard let outputStringsFilesIndex = outputStringsFilesIndexOptional else {
+        print("missing output key '\(outputStringsFilesArguments[0])' or '\(outputStringsFilesArguments[1])'")
+        return
+    }
+    
+    guard outputStringsFilesIndex+1 <= Process.arguments.count else {
+        print("missing input path after key '\(outputStringsFilesArguments[0])' or '\(outputStringsFilesArguments[1])'")
+        return
+    }
+
+    let outputStringsFilesPaths = Process.arguments[outputStringsFilesIndex+1].componentsSeparatedByString(",")
+    
+
+    print("Trying to extract strings from Storyboard at path '\(inputStoryboardPath)' to Strings files at paths '\(outputStringsFilesPaths)'...")
+}
+
+run()
