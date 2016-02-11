3d89f8e7dc954d244aad0264fcb84f9e5941abf8
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-02-11 21:02:30 +0100

    Duplicate test to cover both empty and non-empty values cases

--- Tests/Code/StringsFileReaderTests.swift
+++ Tests/Code/StringsFileReaderTests.swift
@@ -18,10 +18,56 @@ class StringsFileReaderTests: XCTestCase {
         } catch {
             print("Could not cleanup TestExample.strings")
         }
+    }
+    
+    func testExampleStringsFileWithEmptyNewValues() {
+        let oldStringsFilePath = "\(PROJECT_DIR)/Tests/Assets/StringsFiles/OldExample.strings"
+        let newStringsFilePath = "\(PROJECT_DIR)/Tests/Assets/StringsFiles/NewExample.strings"
+        let testStringsFilePath = "\(PROJECT_DIR)/Tests/Assets/StringsFiles/TestExample.strings"
+        
+        do {
+            try NSFileManager.defaultManager().copyItemAtPath(oldStringsFilePath, toPath: testStringsFilePath)
+            let stringsFileUpdater = StringsFileUpdater(path: testStringsFilePath)!
+            
+            let expectedLinesBeforeIncrementalUpdate = [
+                "",
+                "/* Class = \"UIButton\"; normalTitle = \"Example Button 1\"; ObjectID = \"35F-cl-mdI\"; */",
+                "\"35F-cl-mdI.normalTitle\" = \"Example Button 1\";",
+                "",
+                "/* Class = \"UIButton\"; normalTitle = \"Example Button 2\"; ObjectID = \"COa-YO-eGf\"; */",
+                "\"COa-YO-eGf.normalTitle\" = \"Example Button 2\";",
+                "",
+                "/* Class = \"UIButton\"; normalTitle = \"Example Button 3\"; ObjectID = \"cHL-Zc-L39\"; */",
+                "\"cHL-Zc-L39.normalTitle\" = \"Example Button 3\";",
+                ""
+            ]
+            
+            XCTAssertEqual(stringsFileUpdater.linesInFile, expectedLinesBeforeIncrementalUpdate)
+            
+            stringsFileUpdater.incrementallyUpdateKeys(withStringsFileAtPath: newStringsFilePath)
+            
+            let expectedLinesAfterIncrementalUpdate = [
+                "",
+                "/* Class = \"UIButton\"; normalTitle = \"Example Button 1\"; ObjectID = \"35F-cl-mdI\"; */",
+                "\"35F-cl-mdI.normalTitle\" = \"Example Button 1\";",
+                "",
+                "/* Class = \"UIButton\"; normalTitle = \"Example Button 2\"; ObjectID = \"COa-YO-eGf\"; */",
+                "\"COa-YO-eGf.normalTitle\" = \"Example Button 2\";",
+                "",
+                "/* Class = \"UIButton\"; normalTitle = \"Example Button 4\"; ObjectID = \"xyz-12-345\"; */",
+                "\"xyz-12-345.normalTitle\" = \"\";",
+                ""
+            ]
+            
+            XCTAssertEqual(stringsFileUpdater.linesInFile, expectedLinesAfterIncrementalUpdate)
+            
+        } catch {
+            XCTAssertTrue(false, (error as NSError).description)
+        }
         
     }
     
-    func testExampleStringsFile() {
+    func testExampleStringsFileWithPrefilledNewValues() {
         let oldStringsFilePath = "\(PROJECT_DIR)/Tests/Assets/StringsFiles/OldExample.strings"
         let newStringsFilePath = "\(PROJECT_DIR)/Tests/Assets/StringsFiles/NewExample.strings"
         let testStringsFilePath = "\(PROJECT_DIR)/Tests/Assets/StringsFiles/TestExample.strings"
