1cfcb0c318e0d3e5cbbb07fa7976d89893ec90b0
Cihat Gündüz <CihatGuenduez@posteo.de>
2016-08-27 13:59:03 +0200

    Check that already existing keys aren't sorted by default

---
+++ Tests/Assets/Code Files/UnsortedKeys/SwiftExample3.swift
@@ -0,0 +1,19 @@
+//
+//  SwiftExample3.swift
+//  BartyCrouch
+//
+//  Created by Cihat Gündüz on 27.08.16.
+//  Copyright © 2016 Flinesoft. All rights reserved.
+//
+
+import Foundation
+
+class SwiftExample3 {
+
+    func exampleFunction1() {
+
+        NSLocalizedString("ccc", comment: "")
+
+    }
+
+}
--- Tests/Code/CommandLineActorTests.swift
+++ Tests/Code/CommandLineActorTests.swift
@@ -11,7 +11,55 @@ import XCTest
 @testable import BartyCrouch
 
 class CommandLineActorTests: XCTestCase {
+
+    // MARK: - Stored Properties
+
+    static let stringsFilesDirPath = "\(BASE_DIR)/Tests/Assets/Strings Files"
+
+    let codeFilesDirPath = "\(BASE_DIR)/Tests/Assets/Code Files/UnsortedKeys"
+    let unsortedKeysStringsFilePath = "\(stringsFilesDirPath)/UnsortedKeys/Base.lproj/Localizable.strings"
+    let unsortedKeysDirPath = "\(stringsFilesDirPath)/UnsortedKeys"
+
+
+    // MARK: - Test Callbacks
+
+    override func setUp() {
+        if NSFileManager.defaultManager().fileExistsAtPath(unsortedKeysStringsFilePath + ".backup") {
+            try! NSFileManager.defaultManager().removeItemAtPath(unsortedKeysStringsFilePath + ".backup")
+        }
+        try! NSFileManager.defaultManager().copyItemAtPath(unsortedKeysStringsFilePath, toPath: unsortedKeysStringsFilePath + ".backup")
+    }
+
+    override func tearDown() {
+        try! NSFileManager.defaultManager().removeItemAtPath(unsortedKeysStringsFilePath)
+        try! NSFileManager.defaultManager().copyItemAtPath(unsortedKeysStringsFilePath + ".backup", toPath: unsortedKeysStringsFilePath)
+        try! NSFileManager.defaultManager().removeItemAtPath(unsortedKeysStringsFilePath + ".backup")
+    }
+
+
+    // MARK: - Test Methods
+
+    func testActOnCode() {
+
+        let args = ["bartycrouch", "code", "-p", codeFilesDirPath, "-l", unsortedKeysDirPath, "-a"]
+        CommandLineParser(arguments: args).parse { (commonOptions, subCommandOptions) in
+            CommandLineActor().act(commonOptions, subCommandOptions: subCommandOptions)
+
+            guard let updater = StringsFileUpdater(path: self.unsortedKeysStringsFilePath) else {
+                XCTFail()
+                return
+            }
+
+            let resultingKeys = updater.findTranslationsInLines(updater.linesInFile).map { $0.key }
+            let expectedKeys = ["DDD", "ggg", "BBB", "aaa", "FFF", "eee", "ccc"]
+
+            XCTAssertEqual(resultingKeys, expectedKeys)
+        }
+
+    }
     
-    // TODO: not yet implemented
+    // TODO: tests for actOnInterfaces not yet implemented
+
+    // TODO: tests for actOnTranslate not yet implemented
     
 }
