bd547e11e77b8358b4e20b2307b8b3eb0d5d17e0
Cihat Gündüz <commits@cihatguenduez.de>
2018-11-05 16:58:00 +0100

    Add theoretical support for SPM & update Podspec

--- Package.swift
+++ Package.swift
@@ -1,26 +1,37 @@
-// swift-tools-version:4.0
+// swift-tools-version:4.2
 import PackageDescription
 
 let package = Package(
     name: "BartyCrouch",
     products: [
-        .executable(name: "bartycrouch", targets: ["BartyCrouch CLI"])
+        .executable(name: "bartycrouch", targets: ["BartyCrouch"]),
+        .library(name: "BartyCrouchKit", targets: ["BartyCrouchKit"])
     ],
     dependencies: [
+        .package(url: "https://github.com/jatoben/CommandLine.git", .upToNextMajor(from: "2.0.0")),
+        .package(url: "https://github.com/Flinesoft/HandySwift.git", .upToNextMajor(from: "2.6.0")),
+        .package(url: "https://github.com/Dschee/Polyglot.git", .branch("master")),
+        .package(url: "https://github.com/onevcat/Rainbow.git", .upToNextMajor(from: "3.1.4")),
     ],
     targets: [
         .target(
-            name: "BartyCrouch CLI",
-            dependencies: [],
-            path: ".",
-            sources: [
-                "BartyCrouch CLI/",
-                "Sources/Code",
-                "Carthage/CommandLine",
-                "Carthage/HandySwift",
-                "Carthage/Polyglot"
-            ]
+            name: "BartyCrouch",
+            dependencies: ["BartyCrouchKit"],
+            path: "Sources/BartyCrouch",
+            exclude: ["Sources/SupportingFiles"]
+        ),
+        .target(
+            name: "BartyCrouchKit",
+            dependencies: ["CommandLine", "HandySwift", "Polyglot", "Rainbow"],
+            path: "Sources/BartyCrouchKit",
+            exclude: ["Sources/SupportingFiles"]
+        ),
+        .testTarget(
+            name: "BartyCrouchKitTests",
+            dependencies: ["BartyCrouchKit"],
+            path: "Tests/BartyCrouchKitTests",
+            exclude: ["Tests/SupportingFiles"]
         )
     ],
-    swiftLanguageVersions: [4]
+    swiftLanguageVersions: [.v4_2]
 )
--- Sources/BartyCrouch/main.swift
+++ Sources/BartyCrouch/main.swift
@@ -1,6 +1,7 @@
 //  Created by Cihat Gündüz on 10.02.16.
 
 import Foundation
+import BartyCrouchKit
 
 func run() {
     CommandLineParser().parse { commonOptions, subCommandOptions in
