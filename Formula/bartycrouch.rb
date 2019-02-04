1b691d262ea3f1e3f2141e89b6f068d5bb75f02c
Cihat Gündüz <commits@cihatguenduez.de>
2019-02-04 16:18:21 +0100

    Fix Homebrew formula to make Core-compatible

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -1,5 +1,5 @@
 class Bartycrouch < Formula
-  desc "Localization/I18n: Incrementally update/translate your Strings files from .swift, .h, .m(m), .storyboard or .xib files."
+  desc "Incrementally update/translate your Strings files"
   homepage "https://github.com/Flinesoft/BartyCrouch"
   url "https://github.com/Flinesoft/BartyCrouch.git", :tag => "4.0.0", :revision => "3afdce4b875b6e8a573eaa30a225e709b7ee7b0a"
   head "https://github.com/Flinesoft/BartyCrouch.git"
@@ -11,6 +11,23 @@ class Bartycrouch < Formula
   end
 
   test do
-    system "#{bin}/bartycrouch"
+    (testpath/"Test.swift").write <<~EOS
+      import Foundation
+
+      class Test {
+        func test() {
+            NSLocalizedString("test", comment: "")
+        }
+      }
+    EOS
+
+    (testpath/"en.lproj/Localizable.strings").write <<~EOS
+      /* No comment provided by engineer. */
+      "oldKey" = "Some translation";
+    EOS
+
+    system bin/"bartycrouch", "update"
+    assert_match /"oldKey" = "/, File.read("en.lproj/Localizable.strings")
+    assert_match /"test" = "/, File.read("en.lproj/Localizable.strings")
   end
 end
