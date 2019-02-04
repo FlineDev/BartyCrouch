e41e0cbe0607fd147d40fff6fe3c907cdc6d8f0b
Cihat Gündüz <commits@cihatguenduez.de>
2019-02-04 15:52:11 +0100

    Update formula to version 4.0.0

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -1,7 +1,7 @@
 class Bartycrouch < Formula
   desc "Localization/I18n: Incrementally update/translate your Strings files from .swift, .h, .m(m), .storyboard or .xib files."
   homepage "https://github.com/Flinesoft/BartyCrouch"
-  url "https://github.com/Flinesoft/BartyCrouch.git", :tag => "4.0.0-alpha.2", :revision => "7edc4cfdcdf934a823914b3b0332c007557c9609"
+  url "https://github.com/Flinesoft/BartyCrouch.git", :tag => "4.0.0", :revision => "3afdce4b875b6e8a573eaa30a225e709b7ee7b0a"
   head "https://github.com/Flinesoft/BartyCrouch.git"
 
   depends_on :xcode => ["10.0", :build]
