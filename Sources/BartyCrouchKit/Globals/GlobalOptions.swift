e016720e73ff9514725c748dcc834bed318d2f4a
Cihat Gündüz <commits@cihatguenduez.de>
2020-04-24 09:49:56 +0200

    Document changes of PR #181

--- Sources/BartyCrouchKit/Globals/GlobalOptions.swift
+++ Sources/BartyCrouchKit/Globals/GlobalOptions.swift
@@ -5,7 +5,7 @@ public enum GlobalOptions {
     static let verbose = Flag("-v", "--verbose", description: "Prints more detailed information about the executed command")
     static let xcodeOutput = Flag("-x", "--xcode-output", description: "Prints warnings & errors in Xcode compatible format")
     static let failOnWarnings = Flag("-w", "--fail-on-warnings", description: "Returns a failed status code if any warning is encountered")
-    static let path = Key<String>("-p", "--path", description: "Specifies a different path than current to run BartyCrouch from there.")
+    static let path = Key<String>("-p", "--path", description: "Specifies a different path than current to run BartyCrouch from there")
 
     public static var all: [Option] {
         return [verbose, xcodeOutput, failOnWarnings, path]
