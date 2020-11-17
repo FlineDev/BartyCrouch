14f5f24f5e0d32845feebb9b88164e2e71b907c3
Cihat Gündüz <commits@cihatguenduez.de>
2020-11-17 12:38:05 +0100

    Add tests on Linux platform

---
+++ Tests/LinuxMain.swift
@@ -0,0 +1,18 @@
+// Generated using Sourcery 1.0.0 — https://github.com/krzysztofzablocki/Sourcery
+// DO NOT EDIT
+
+
+@testable import BartyCrouchTranslatorTests
+import XCTest
+
+// swiftlint:disable line_length file_length
+
+extension MicrosoftTranslatorApiTests {
+    static var allTests: [(String, (MicrosoftTranslatorApiTests) -> () throws -> Void)] = [
+        ("testTranslate", testTranslate)
+    ]
+}
+
+XCTMain([
+    testCase(MicrosoftTranslatorApiTests.allTests)
+])
