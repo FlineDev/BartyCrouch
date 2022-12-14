88bf3e9413b7b01fa3d5522ad99823828521e88d
Cihat Gündüz <commits@cihatguenduez.de>
2022-12-14 21:17:07 +0900

    Update formula to version 4.14.0

--- Formula/bartycrouch.rb
+++ Formula/bartycrouch.rb
@@ -1,7 +1,7 @@
 class Bartycrouch < Formula
   desc "Incrementally update/translate your Strings files"
   homepage "https://github.com/Flinesoft/BartyCrouch"
-  url "https://github.com/Flinesoft/BartyCrouch.git", :tag => "4.13.0", :revision => "36cc46399abe717986cda40ccd6d6eb34c6ff70d"
+  url "https://github.com/Flinesoft/BartyCrouch.git", :tag => "4.14.0", :revision => "03c3816a39ac2d82fe9a2f2efc84949a1e50165d"
   head "https://github.com/Flinesoft/BartyCrouch.git"
 
   depends_on :xcode => ["14.0", :build]
