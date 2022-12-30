b1a8796c75f03f2df459a7f6a126a038574aa00c
Cihat Gündüz <commits@cihatguenduez.de>
2022-12-31 00:37:47 +0900

    Make new 4.14.1 release, for CocoaPods only

--- Sources/BartyCrouch/main.swift
+++ Sources/BartyCrouch/main.swift
@@ -5,7 +5,7 @@ import SwiftCLI
 // MARK: - CLI
 let cli = CLI(
   name: "bartycrouch",
-  version: "4.14.0",
+  version: "4.14.1",
   description: "Incrementally update & translate your Strings files from code or interface files."
 )
 
